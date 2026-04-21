16d1f46cbe8dffcd31a1eef54133d70132c3cbd1
kostiantyngarbar <kostiantyn.garbar@prisma-capacity.eu>
2026-04-21 13:57:21 +0200

    fix

--- src/app/plans-and-pricing/page.tsx
+++ src/app/plans-and-pricing/page.tsx
@@ -2,7 +2,7 @@
 import { Navbar, Footer } from "@/components";
 
 // sections
-import Prices from "../prices";
+import {Prices} from "../prices";
 import Faq from "../faq";
 
 export default function PricingPage() {
